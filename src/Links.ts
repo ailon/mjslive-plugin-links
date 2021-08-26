7dae6888b0dc23bb0e828343dd21e9dcea3b92d8
Alan Mendelevich <ailon@ailon.org>
2021-08-26 15:48:24 +0300

    add chaning cursor style to pointer on markers with links

--- src/Links.ts
+++ src/Links.ts
@@ -2,6 +2,7 @@ import {
   MarkerView,
   IMarkerViewPlugin,
   PointerEventHandler,
+  MarkerViewEventHandler,
 } from 'markerjs-live';
 
 export class Links implements IMarkerViewPlugin {
@@ -20,16 +21,35 @@ export class Links implements IMarkerViewPlugin {
   public init(markerView: MarkerView): void {
     this.markerView = markerView;
 
+    this.markerView.addEventListener('load', this.markersLoaded);
     this.markerView.addEventListener('pointerdown', this.markerClicked);
+  }
+
+  private markersLoaded: MarkerViewEventHandler = (markerView) => {
+    markerView.markers.forEach(m => {
+      // change cursor style on markers containing links
+      if (this.getLink(m.notes)) {
+        m.container.style.cursor = 'pointer';
+      }
+    });
+  }
+
+  private getLink = (notes?: string): string | undefined => {
+    if (notes !== undefined) {
+      const linkExp = new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_+.~#?&//=]*)/gm);
+      const result = linkExp.exec(notes);
+      return (result && result.length > 0) ? result[0] : undefined;
+    } else {
+      return undefined;
+    }
   }
 
   private markerClicked: PointerEventHandler = (markerView, ev, marker) => {
     if (marker !== undefined && marker.notes !== undefined) {
-      const linkExp = new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_+.~#?&//=]*)/gm);
-      const result = linkExp.exec(marker.notes);
-      if (result && result.length > 0) {
+      const linkURL = this.getLink(marker.notes);
+      if (linkURL !== undefined) {
         const link = document.createElement('a');
-        link.href = result[0];
+        link.href = linkURL;
         link.target = this.target;
         link.click();
       }
